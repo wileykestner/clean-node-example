0ecf508d0cb882c64f11abc3a69211baf08d1442
Wiley Kestner <wiley@aya.yale.edu>
2016-04-06 21:06:20 -0700

    Rename / reorganize PresentDishes

--- app/PresentDishes.js
+++ app/PresentDishes.js
@@ -3,15 +3,16 @@
 var exports = module.exports = {};
 
 exports.new = function (dependencies) {
-  return {
-    execute: execute,
-    _dishRepository: dependencies.dishRepository
-  };
+  var dishRepository =  dependencies.dishRepository;
 
   function execute (observer) {
     var success = function (allDishes) {
         observer.didPresentDishes(allDishes);
     };
-    this._dishRepository.fetchAllDishes(success);
+    dishRepository.fetchAllDishes(success);
   }
+
+  return {
+    execute: execute
+  };
 };
