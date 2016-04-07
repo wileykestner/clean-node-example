bc24d215a07e191954b659681580cf4b93ab8386
Wiley Kestner <wiley@aya.yale.edu>
2016-04-06 21:31:33 -0700

    Add execute describe in PresentDishesSpec

--- spec/PresentDishesSpec.js
+++ spec/PresentDishesSpec.js
@@ -15,37 +15,43 @@ describe("PresentDishes", function () {
         subject = PresentDishes.new(dependencies);
     });
 
-    describe("when no dishes have been created", function () {
-        it("presents an empty list of dishes to an observer", function () {
-            var presentedDishes = null;
-            var observer = {"didPresentDishes": function (dishes) {
-                presentedDishes = dishes;
-            }};
+    describe("execute", function (){
+        describe("when no dishes have been created", function () {
+            it("presents an empty list of dishes to an observer", function () {
+                var presentedDishes = null;
+                var observer = {"didPresentDishes": function (dishes) {
+                    presentedDishes = dishes;
+                }};
 
-            subject.execute(observer);
+                subject.execute(observer);
 
-            var expectedDishes = [];
-            expect(presentedDishes).toEqual(expectedDishes);
-        });
-    });
-
-    describe("when one dish has been created", function () {
-        beforeEach(function (){
-            var emptySuccess = function(){};
-            var emptyFailure = function(){};
-            dishRepository.createDish("Pad See Ew", emptySuccess, emptyFailure);
+                var expectedDishes = [];
+                expect(presentedDishes).toEqual(expectedDishes);
+            });
         });
 
-        it("presents a list containing the single created dish to an observer", function () {
-            var presentedDishes = null;
-            var observer = {"didPresentDishes": function (dishes) {
-                presentedDishes = dishes;
-            }};
+        describe("when one dish has been created", function () {
+            var observer;
+            var presentedDishes;
 
-            subject.execute(observer);
-            var expectedDish = {identifier: 0, name: "Pad See Ew"};
-            var expectedDishes = [expectedDish];
-            expect(presentedDishes).toEqual(expectedDishes);
+            beforeEach(function (){
+                var emptySuccess = function(){};
+                var emptyFailure = function(){};
+                dishRepository.createDish("Pad See Ew", emptySuccess, emptyFailure);
+
+                presentedDishes = null;
+                observer = {"didPresentDishes": function (dishes) {
+                    presentedDishes = dishes;
+                }};
+
+                subject.execute(observer);
+            });
+
+            it("presents a list containing the single created dish to an observer", function () {
+                var expectedDish = {identifier: 0, name: "Pad See Ew"};
+                var expectedDishes = [expectedDish];
+                expect(presentedDishes).toEqual(expectedDishes);
+            });
         });
     });
 });
